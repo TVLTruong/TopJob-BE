58e174045624b73eac1139cfb1bc63690b635c81
LamTruong <tvlamtruong@gmail.com>
2025-11-03 08:42:16 +0700

    Recreate project with NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
-import { AuthModule } from './auth/auth.module';
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [User],
-        synchronize: true,
-      }),
-    }),
 
-    UserModule,
-    AuthModule,
-  ],
+@Module({
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors();
-
-  // Bật validation cho toàn bộ DTO
-  app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(4000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
