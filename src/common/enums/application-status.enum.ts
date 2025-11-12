287e47b95b3fc5fde94ff39b85c7e807ba3135f8
LamTruong <tvlamtruong@gmail.com>
2025-11-12 14:19:31 +0700

    create some new enum

---
+++ src/common/enums/application-status.enum.ts
@@ -0,0 +1,6 @@
+export enum ApplicationStatus {
+  NEW = 'NEW', // Ứng viên mới nộp đơn
+  VIEWED = 'VIEWED', // Nhà tuyển dụng đã xem hồ sơ
+  SHORTLISTED = 'SHORTLISTED', // Ứng viên được đưa vào danh sách "phù hợp"
+  REJECTED = 'REJECTED', // Ứng viên bị từ chối
+}
---
+++ src/common/enums/experience-level.enum.ts
@@ -0,0 +1,9 @@
+export enum ExperienceLevel {
+  INTERN = 'INTERN',
+  FRESHER = 'FRESHER',
+  JUNIOR = 'JUNIOR',
+  MIDDLE = 'MIDDLE',
+  SENIOR = 'SENIOR',
+  LEAD = 'LEAD',
+  MANAGER = 'MANAGER',
+}
