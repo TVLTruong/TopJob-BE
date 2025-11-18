2d6c0a14deef534e5db5242b0ff08c5b8d14e8c9
LamTruong <tvlamtruong@gmail.com>
2025-11-18 08:27:50 +0700

    update configuration.ts

---
+++ src/config/configuration.ts
@@ -0,0 +1,22 @@
+// src/config/configuration.ts
+export default () => ({
+  port: parseInt(process.env.PORT || '3000', 10),
+  database: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRATION,
+  },
+  smtp: {
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT || '587', 10),
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+    from: process.env.SMTP_FROM,
+  },
+});
